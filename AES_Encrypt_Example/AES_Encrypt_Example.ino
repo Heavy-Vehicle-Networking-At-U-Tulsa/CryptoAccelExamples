2984d4cfd6b11992208b13b41c4f805da747302e
benettlinger <39038496+benettlinger@users.noreply.github.com>
2019-05-14 21:47:48 -0500

    Add files via upload

---
+++ AES_Encrypt_Example/AES_Encrypt_Example.ino
@@ -0,0 +1,26 @@
+#include "CryptoAccel.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(5000);
+  Serial.println("Starting :)");
+  unsigned int i;
+  unsigned char aeskey[16];
+  for (i = 0; i < sizeof(aeskey); i++)  aeskey[i] = 0x70;
+  unsigned char keysched[4*44];
+  mmcau_aes_set_key(aeskey, 128, keysched);
+  unsigned char in[16];
+  for (i = 0; i < sizeof(in); i++)  in[i] = i;
+  for (i = 0; i < sizeof(in); i++)  Serial.print(in[i]);
+  Serial.println();
+  unsigned char out[16];
+  mmcau_aes_encrypt(in, keysched, 10, out);
+  for (i = 0; i < sizeof(out); i++) Serial.print(out[i]);
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
